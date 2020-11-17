d0c222e489833ab5faec833c34cca3fd5d6d9359
gr99182 <gr99182@LR009MMC>
2020-11-17 13:41:37 -0600

    Add Glenn 2 Class

---
+++ notTHATkindOfDungeon/Models/Glenn2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace notTHATkindOfDungeon.Models
+{
+    public class Glenn2
+    {
+        // This is a sample file
+
+    }
+}
