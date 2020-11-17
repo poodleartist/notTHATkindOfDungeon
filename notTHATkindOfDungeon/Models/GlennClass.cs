6d6a20bc398452bbb0e7a8529ab08609b28b0361
gr99182 <gr99182@LR009MMC>
2020-11-17 13:31:25 -0600

    Added Glenn Class

---
+++ notTHATkindOfDungeon/Models/GlennClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace notTHATkindOfDungeon.Models
+{
+    public class GlennClass
+    {
+        // Good stuff goes here
+    }
+}
