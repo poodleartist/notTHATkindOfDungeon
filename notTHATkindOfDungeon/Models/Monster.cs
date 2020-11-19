56af87b5713b9d830ba5cf564e39e89334977c2a
Andrew O'Connor <ao708051@southeast.edu>
2020-11-19 14:21:30 -0600

    Added Monster class.

---
+++ notTHATkindOfDungeon/Models/Monster.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace notTHATkindOfDungeon.Models
+{
+    public class Monster
+    {
+//      ~ PROPERTY ~                                        ~ EXAMPLE / DESCRIPTION ~ 
+//      -----------------------------------------------------------------------------------------------------
+//
+//      ~ Details ~        
+        public string Race { get; set; }                    // Goblin, Skeleton, etc.
+        public string Description { get; set; }             // Displayed with the "examine" command.
+                                                            // e.g. "He doesn't look friendly"
+        public string Name { get; set; }                    // Typically, this will be for bosses.
+
+//      ~ Stats ~
+        public int Health { get; set; }
+        public int Attack { get; set; }
+        // public int Defense { get; set; }                 // Defense isn't necessary, but if we decide
+                                                            // to implement it, it will be the amount of
+                                                            // damage subtracted from the player's attack.
+
+        // public Item ItemDropped { get; set; }            // Will create an Item object later.
+
+        
+
+
+
+
+
+
+
+
+
+
+    }
+}
